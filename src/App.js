3fc4c674b62f1b66a887524810a971a42ba77a3a
Johnny <JohnKnee@users.noreply.github.com>
2019-03-20 19:05:40 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './App.css';
+import LoginTabs from './components/TabContainer'
+import Home from './components/Home';
+import fire from './firebase/Fire';
 
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#00b2ff',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#0066ff',
+      main: '#0044ff',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
+    },
+    // error: will use the default color
+  },
+});
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user: {},
+    }
+  }
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({user});
+      } else {
+        this.setState({user: null});
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.authListener();
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+       
+       {this.state.user ? <Home user={this.state.user}/> : <LoginTabs />}
+        
+        </MuiThemeProvider >
     );
   }
 }
