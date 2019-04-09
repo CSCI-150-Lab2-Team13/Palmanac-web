b3782cbd1785295b977185a7a702df38910bd6cd
Johnny <JohnKnee@users.noreply.github.com>
2019-04-08 20:56:37 -0700

    fix quick menu to handle custom recurrence and change styling

--- src/components/RecurrenceMenu.js
+++ src/components/RecurrenceMenu.js
@@ -2,6 +2,7 @@ import React,  { Fragment, useState } from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 import Select from '@material-ui/core/Select';
 import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
@@ -9,33 +10,32 @@ import Autorenew from '@material-ui/icons/Autorenew';
 import AutorenewOutlined from '@material-ui/icons/AutorenewOutlined';
 import moment from 'moment';
 import {RRule} from 'rrule';
-
-function pront(e) {
-    console.log(e.target.value);
-}
-function recurRule(freq, day) {
-    if (freq == "Custom"){
-        console.log(freq);
-    }
-    else if (freq !== 'none') {
-        const rule = new RRule({
-            freq: freq,
-            dtstart: new Date(moment(day).utc()),
-            count: 5
-        });
-        
-        rule.all().forEach(day => {
-            console.log(moment(day).format());
-        });
-    };
-}
+import CustomRecurrence from './CustomRecurrence';
 
 function RecurrenceMenu(props) {
     const dayOfWeek = moment(props.day).format("dddd");
     const dayOfMonth = moment(props.day).format("MMMM D");
-    const [recurrence, setRecurrence] = useState('none');
-    recurRule(recurrence, props.day);
+    const startDay = moment(props.day).format();
+    const [recurrence, setRecurrence] = useState('');
+    const [occurences, setOccurences] = useState(0);
+    const [customRecurr, setCustomRecurr] = useState(false);
+    const recurRule = (freq, occur) => {
+        setRecurrence(freq);
+        setOccurences(occur)
 
+        if(freq == 'Custom'){
+            setCustomRecurr(true);
+        } else if (freq !== '') {
+            const rule = new RRule({
+                freq: freq,
+                dtstart: new Date(moment(props.day).utc()),
+                count: occur
+            });
+            console.log(rule.toString());
+            props.setRrule(rule.toString());
+        }    
+        
+    }
 
     return(
         <Fragment>
@@ -43,14 +43,16 @@ function RecurrenceMenu(props) {
                 icon={<AutorenewOutlined />} 
                 checkedIcon={<Autorenew/>} 
                 onChange={props.handleCheck('recurring')}
+                style={{paddingLeft: 0}}
             />
             <FormControl variant="filled">
                 <InputLabel >Recurring</InputLabel> 
                     <Select
                         value={recurrence}
-                        onChange={e=>setRecurrence(e.target.value)}
+                        onChange={e=>recurRule(e.target.value, occurences)}
                         disabled={!props.recurring}
-                        autoWidth
+                        style = {{width: 200}}
+                        autoWidth={true}
                         input={<Input/>}
                     >
                         <MenuItem value={RRule.DAILY}>Daily</MenuItem>
@@ -58,7 +60,22 @@ function RecurrenceMenu(props) {
                         <MenuItem value={RRule.YEARLY}>Annually on {dayOfMonth}</MenuItem>
                         <MenuItem value="Custom"><em>Custom</em></MenuItem>
                     </Select>
-            </FormControl>   
+            </FormControl>
+            <TextField
+            disabled={!props.recurring}
+                id="outlined-number"
+                style = {{width: 50}}
+                label="Recurrences"
+                value={occurences}
+                onChange={e => recurRule(recurrence, e.target.value)}
+                type="number"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                margin="none"
+                
+        />
+        {customRecurr ? <CustomRecurrence open={customRecurr} start={startDay} setRrule={props.setRrule} />: null}   
         </Fragment>
     );
 }
