05c1f88929fc20c0bbe482f151fe89228e10adbd
Mvalov <mvalov18@mail.fresnostate.edu>
2019-04-30 20:30:33 -0700

    Profile tab finalized

--- src/components/CalendarProfile.js
+++ src/components/CalendarProfile.js
@@ -5,8 +5,10 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { firestore } from 'firebase';
+import Text from 'react';
+//import { firestore } from 'firebase';
 import firebase from 'firebase/app';
+import './CalendarProfileLook.css';
 
 class CalendarProfile extends React.Component {
   state = {
@@ -15,46 +17,44 @@ class CalendarProfile extends React.Component {
     lastName: "",
     email: "",
     photoURL: "",
-    user:  firebase.auth().currentUser.displayName,
+    user: firebase.auth().currentUser.displayName,
+    errormessage: null,
   };
 
   componentWillMount = () => {
       this.setState({ open: true});
-      const ref = firebase.firestore().collection('users').doc(this.props.user);
+
+      const ref = firebase.firestore().collection('users').doc(this.state.user);
       ref.get().then(doc => {
         if (doc.exists) {
           let data = doc.data()
-          this.setState({firstName: data.firstName, lastName: data.lastname, photoURL: data.photoURL})
+          this.setState({firstName: data.firstName, lastName: data.lastname, photoURL: data.photoURL, email: data.Email})
         }
         else { console.error("No such user!"); }
       })
-      //.catch(error) {console.error("Error getting user: ", error)}
-  };
-
-  getEmail(){
-    const ref = firebase.firestore().collection('users').doc(this.state.user);
-    ref.get().then(doc => {
-      if (doc.exists) {
-        let data = doc.data()
-        this.setState({firstName: data.firstName, lastName: data.lastname, photoURL: data.photoURL})
-      }
-      else { console.error("No such user!"); }
-    })
-    //.catch(error) {console.error("Error getting user: ", error)}
+      .catch((error)=>
+      this.setState({errormessage:error}));
   };
 
   render() {
     return (
       <div>
+        {this.state.errorMessage &&
+            <Text style={{ color: 'red', textAlign: 'center', marginTop: 5 }}>
+                {this.state.errorMessage}
+            </Text>
+        }
         <Dialog
           open={this.state.open}
           onClose={this.props.toggle}
           aria-labelledby="profile-dialog-title"
           aria-describedby="profile-dialog-description"
         >
-          <DialogTitle id="profile-dialog-title">{"Profile"}</DialogTitle>
+          <DialogTitle id="profile-dialog-title">{"Profile: "}</DialogTitle>
           <DialogContent>
-            <DialogContentText id="profile-dialog-firstname"> Name: {this.state.firstName} + {this.state.lastName} </DialogContentText>
+            <img src= {this.state.photoURL} />
+            <br></br>
+            <DialogContentText id="profile-dialog-firstname"> Name: {this.state.firstName} {this.state.lastName} </DialogContentText>
             <br></br>
             <DialogContentText id="profile-dialog-email"> Email: {this.state.email} </DialogContentText>
           </DialogContent>
@@ -66,5 +66,7 @@ class CalendarProfile extends React.Component {
     );
   }
 }
+
+
 
 export default CalendarProfile;
