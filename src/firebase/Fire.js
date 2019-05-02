e683a223abf3ae5e9272ed927ed359d597632406
Johnny <JohnKnee@users.noreply.github.com>
2019-05-02 16:29:11 -0700

    fire credentials

--- src/firebase/Fire.js
+++ src/firebase/Fire.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+  apiKey: "AIzaSyCbC-n--mjbOUSWOoTbjyxQcthtV7m5xhQ",
+  authDomain: "scheduleapp-boof.firebaseapp.com",
+  databaseURL: "https://scheduleapp-boof.firebaseio.com",
+  projectId:"scheduleapp-boof",
+  storageBucket: "scheduleapp-boof.appspot.com",
+  messagingSenderId: "48199855930"
   };
 const fire = firebase.initializeApp(config);
 export default fire;
